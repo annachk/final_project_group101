b1570f4e23b28a87cd68bf2d54bd3d35db00617c
Anna Kafrune <annachk@terpmail.umd.edu>
2020-11-09 11:47:32 -0500

    Created skeleton of 4 additional functions (generate_password, report_unaccepted_suggestion, report_password, level_of_personalization)

--- password_generator.py
+++ password_generator.py
@@ -5,8 +5,10 @@ import re
 
 def get_passwords(filename):
     ''' Read content from file and put it in two lists, one for whole passwords and another for parts of passwords
+    
     Args:
         filename(str): path to a file to be read in
+    
     Returns:
         all_same(list): contains whole regularly used passwords breached to access sensitive information 
         all_too_similar(list): contains parts of regularly used passwords breached to access sensitive information
@@ -20,13 +22,12 @@ def get_passwords(filename):
         all_same = [same_words for same_words in re.findall(same, content) if len(same_words) > 0]
         all_too_similar = [part for too_similar_words in re.findall(too_similar, content) for part in too_similar_words if len(too_similar_words) > 0 and part != '']    
         
-        #print(all_same, len(all_same))
-        #print(all_too_similar, len(all_too_similar))
-        
         return all_same, all_too_similar
         
 def evaluate_suggestion(filename, suggestion):
-    ''' Compare the suggestion provided by the user with the contents in each list created in the get_passwords function and evaluate if it passes or not
+    ''' Compare the suggestion provided by the user with the contents in each list created in the get_passwords function 
+        and evaluate if it passes or not (if the suestion is too similar or exactly the same as a password in the .txt file, then reject it)
+    
     Args:
         filename(str): path to a file to be read in
         suggestion(str): user's suggestion of what to include in the generated password
@@ -42,16 +43,32 @@ def evaluate_suggestion(filename, suggestion):
     else:
         print("Your suggestion passed. It will be used when generating your password")
 
+def generate_password():
+    """Generate a random password based on the user’s suggestion or, if the suggestion was not approved, a totally randomized password that agrees with the password requirements
+    
+    """
+
+def report_unaccepted_suggestion():
+    """Report to the user that his/her suggestion was not approved and why; has options to either input another suggestion or get a totally randomized password
+    
+    """
+
+def report_password():
+    """Report to the user what the password is
+    
+    """
+    
+def level_of_personalization():
+    """From user's choice, modify the suggestion based on the classifications of each level of personalization (goes from 1-3):
+        1: sugggestion as a whole in the generated password
+        2: partially recognizable suggestion in the g.p. at a glance
+        3: characters/symbols/numbers syggested are used but they are spplitted into parts and scatttered randomly through the p.g.
+        This function works together with generate_password()
+    
+    """
+
 filename = "100000_regularly_used_passwords_breached.txt"
 suggestion = ""
 
 evaluate_suggestion(filename, suggestion)
-
-
-
-# level of personalization (1-3)
-    # 1: sugggestion as a whole in the generated password
-    # 2: partially recognizable suggestion in the g.p. at a glance
-    # 3: characters/symbols/numbers syggested are used but they are spplitted into parts and scatttered randomly through the p.g.
-
 
