e2c1c0df1303f266e6bd8e10b40a99b311ffe0f8
kvn175 <73406755+kvn175@users.noreply.github.com>
2020-11-10 10:27:48 -0500

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -10,14 +10,14 @@ class Generator:
         self.filename = "100000_regularly_used_passwords_breached.txt"
         
     def get_passwords(self):
-        ''' Read content from file and put it in two lists, one for whole passwords and another for parts of passwords
+        ''' Read content from the file and put it into two lists, one list for whole passwords and another list for parts of passwords
         
         Args:
             filename(str): path to a file to be read in
         
         Returns:
-            all_same(list): contains whole regularly used passwords breached to access sensitive information 
-            all_too_similar(list): contains parts of regularly used passwords breached to access sensitive information
+            all_same(list): contains regulary used passwords previously breached to access sensitive information 
+            all_too_similar(list): contains parts of regularly used passwords previously breached to access sensitive information
         '''
         
         with open(self.filename, "r", encoding = "utf-8") as f:
@@ -50,36 +50,43 @@ class Generator:
             print("Your suggestion passed. It will be used when generating your password")
 
     def generate_password():
-        """Generate a random password based on the user’s suggestion or, if the suggestion was not approved, a totally randomized password that agrees with the password requirements
+        """Generate a random password based on the user’s suggestion or if the suggestion was not approved, 
+        a randomized password will be generated that agrees with the password requirements.
         
         """
+        
 
     def report_unaccepted_suggestion():
-        """Report to the user that his/her suggestion was not approved and why; has options to either input another suggestion or get a totally randomized password
+        """Report to the user that his/her suggestion was not approved and why;
+        allows the input of a new suggestion or generate a randomized password
         
         """
+        
 
     def report_password():
         """Report to the user what the password is
         
         """
         
+        
     def level_of_personalization():
         """From user's choice, modify the suggestion based on the classifications of each level of personalization (goes from 1-3):
-            1: sugggestion as a whole in the generated password
+            1: suggestion as a whole in the generated password
             2: partially recognizable suggestion in the g.p. at a glance
-            3: characters/symbols/numbers syggested are used but they are spplitted into parts and scatttered randomly through the p.g.
+            3: characters/symbols/numbers suggested are used but they are split into parts and scatttered randomly through the p.g.
             This function works together with generate_password()
         
         """
 
+        
 class Manager:        
     def password_manager():
         """Asks the user if they would like to input their newly generated passwords into a text document to keep track of them.
         The user will be able to input the account type, username/email, and generate a password to store on each line in the text file.
-        The user can elect to not use the password manager after they create their passwords."""
+        The user can elect to not use the password manager after they create their passwords.
+        """
 
-
+        
 def main():
     """ Get user's suggestion of what to put in the password, compare the suggestion with regularly used passwords and either reject it or accept it;
         Generate password based on suggestion and level of personalization desired;
