c4c872d26630d383ec818e0416e0f9dbf86ea3a8
kvn175 <73406755+kvn175@users.noreply.github.com>
2020-12-01 18:10:30 -0500

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,4 +1,3 @@
-
 # Source for the list of passwords: https://www.ncsc.gov.uk/news/most-hacked-passwords-revealed-as-uk-cyber-survey-exposes-gaps-in-online-security
 
 import re
@@ -6,24 +5,25 @@ import re
 class Generator:
     """ Get user's suggestion of what to put in the password, 
         compare the suggestion with regularly used passwords and either reject it or accept it;
-        Generate password based on suggestion and level of personalization desired
+        Generate password based on suggestion and level of personalization desired by the user.
     """
+    
     def __init__(self):
         """ Initializes Generator's attributes"""
         
         self.filename = "100000_regularly_used_passwords_breached.txt"
         # all_same stores whole regularly used passwords breached to access sensitive information 
         self.all_same = []
-        # all_too_similar stors parts of regularly used passwords breached to access sensitive information
+        # all_too_similar stores parts of regularly used passwords breached to access sensitive information
         self.all_too_similar = []
         self.get_passwords()
         
     def get_passwords(self):
-        ''' Read content from file and put it in two lists, 
-            one for whole passwords and another for parts of passwords
+        ''' Read content from file and put it into two lists, 
+            one for whole passwords and the other for parts of passwords
         
         Args:
-            filename(str): path to a file to be read in
+            filename(str): the path to a file to be read in
         '''
         
         with open(self.filename, "r", encoding = "utf-8") as f:
@@ -39,7 +39,7 @@ class Generator:
     def evaluate_suggestion(self, suggestion):
         ''' Compare the suggestion provided by the user with the contents in each list created 
             in the get_passwords function and evaluate if it passes or not 
-            (if the suestion is too similar or exactly the same as a password in the .txt file, then reject it)
+            (if the suggestion is too similar or exactly the same as a password in the .txt file, then reject it)
         
         Args:
             filename(str): path to a file to be read in
@@ -62,42 +62,42 @@ class Generator:
         """ Get password requirements, defined by the service provider, from user's input
         
         Returns:
-            requirements (dict): requirements necessary for the password as keys and details as values
+            requirements (dict): the requirements necessary for the password as keys and details as values
         """
         
         requirements = {}
         print('\n------ REQUIREMENTS ------\n'
-                '1 - Min number of caracters\n'
+                '1 - Minimum password character length\n'
                 '2 - Include numbers\n'
                 '3 - Include lowercase and uppercase letters\n'
-                '4 - Have a mix of letters, numbers & symbols\n'
+                '4 - Include mix of letters, numbers, and symbols\n'
                 '5 - Done\n')
         while True:
-            pass_req_query = input('Enter the number of the requirement necessary'
-                                    'for your password found above (one by one):')
+            pass_req_query = input('Enter the number of the requirement(s) necessary for your password found above (one by one):')
             if pass_req_query == '5':
                 break
             elif pass_req_query == '1':
-                min_length = int(input('Min number of caracters: '))
+                min_length = int(input('Enter minimum amount of characters for password: '))
                 requirements[pass_req_query] = min_length
             elif pass_req_query in ['2','3','4']:
                 requirements[pass_req_query] = ''
             else:
-                print('This option is not valid. Try again.')
+                print('Invalid input. Try again (1-5).')
                 continue
         return requirements
     
     def level_of_personalization():
-        """ From user's choice, modify the suggestion based on the classifications 
+        """ From the user's choices, modify the suggestion based on the classifications 
             of each level of personalization (goes from 1-3):
                 1: sugggestion as a whole in the generated password
                 2: partially recognizable suggestion in the g.p. at a glance
                 3: characters/symbols/numbers syggested are used but 
                 they are spplitted into parts and scatttered randomly through the p.g.
             This function works together with generate_password()
-        
         """
-    
+        
+        
+        
     def generate_password():
         """ Generate a random password based on the user’s suggestion and password requirements defined by the user
             or, if the suggestion was not approved, 
@@ -109,8 +109,10 @@ class Generator:
     def report_unaccepted_suggestion():
         """ Report to the user that his/her suggestion was not approved and why; 
             has options to either input another suggestion or get a totally randomized password
-        
+            
+            Returns (str): Informs the user if their suggested password is approved. If denied, a reason for denial is given.
         """
+        
         
     def reset_password():
         """Allows user to reset existing passwords;
@@ -142,6 +144,3 @@ def main():
     
 
 main()    
-
-
-
