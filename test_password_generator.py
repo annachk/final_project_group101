ec356d35f51152c4c895fbb067983e43ac2cd74b
annachk <73417480+annachk@users.noreply.github.com>
2020-12-16 18:50:20 -0500

    Update test_password_generator.py
    added a doctring for the test_get_passwords function

--- test_password_generator.py
+++ test_password_generator.py
@@ -1,10 +1,13 @@
-from pg import Generator
+from password_generator import Generator
 import pytest
 
 g = Generator()
 
 def test_get_passwords():
-    
+    """
+    Test the function get_passwords() - check for type and 
+    the existence of information inside the returned tuple of lists
+    """
     assert isinstance(g.get_passwords(), tuple), "get_passwords() should return a tuple"
     assert isinstance(g.get_passwords()[0], list), "should return a list"
     assert isinstance(g.get_passwords()[1], list), "should return a list"
